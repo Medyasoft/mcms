bcad1830ad52f048b27e94c692230a9b21ba1d19
Yılmaz Can Buhar <yilmaz.buhar@medyasoft.com.tr>
2015-03-05 17:52:44 +0200

    Sql scriptleri eklendi.

---
+++ Plugins/Unigate.Plugins.PageRedirect/SQL/Scripts.sql
@@ -0,0 +1,64 @@
+CREATE TABLE [dbo].[SiteRedirect](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[ContentId] [uniqueidentifier] NOT NULL,
+	[SiteLanguageId] [uniqueidentifier] NOT NULL,
+	[Title] [nvarchar](256) NULL,
+	[Slug] [nvarchar](256) NULL,
+	[CreatedBy] [uniqueidentifier] NOT NULL,
+	[CreateDate] [datetime] NOT NULL,
+	[UpdatedBy] [uniqueidentifier] NOT NULL,
+	[UpdateDate] [datetime] NOT NULL,
+	[LocalAddress] [nvarchar](400) NULL,
+	[RouteAddress] [nvarchar](550) NULL,
+	[TransferQuerystring] [bit] NULL,
+	[OrderId] [int] NULL,
+	[IsTemporary] [bit] NULL,
+	[PageId] [int] NOT NULL,
+ CONSTRAINT [PK_SiteRedirect] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+CREATE TABLE [dbo].[SiteRedirectHistory](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[ContentId] [uniqueidentifier] NOT NULL,
+	[SiteLanguageId] [uniqueidentifier] NOT NULL,
+	[Title] [nvarchar](256) NULL,
+	[Slug] [nvarchar](256) NULL,
+	[CreatedBy] [uniqueidentifier] NOT NULL,
+	[CreateDate] [datetime] NOT NULL,
+	[UpdatedBy] [uniqueidentifier] NOT NULL,
+	[UpdateDate] [datetime] NOT NULL,
+	[LocalAddress] [nvarchar](400) NULL,
+	[RouteAddress] [nvarchar](550) NULL,
+	[TransferQuerystring] [bit] NULL,
+	[OrderId] [int] NULL,
+	[IsTemporary] [bit] NULL,
+	[PageId] [int] NOT NULL,
+ CONSTRAINT [PK_SiteRedirect] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+
+CREATE TRIGGER [dbo].[SiteRedirectChangeTrigger]
+   ON  [dbo].[SiteRedirect]
+   AFTER INSERT,DELETE,UPDATE
+AS 
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	UPDATE [dbo].[SiteMapLastUpdate]
+	SET [SiteRedirectLastUpdateDate] = GETDATE()
+    -- Insert statements for trigger here
+
+END
+GO
+
